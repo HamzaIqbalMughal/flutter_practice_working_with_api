4b3c57cca45d6d56c814001766730881846819ff
HamzaIqbalMughal <humza.iqbal.hi@gmail.com>
2023-11-01 05:06:54 -0700

    added DropDownAPIScreen

---
+++ lib/drop_down_api_screen.dart
@@ -0,0 +1,91 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'Models/dropdown_model.dart';
+
+class DropDownAPIScreen extends StatefulWidget {
+  const DropDownAPIScreen({super.key});
+
+  static const String screen_id = 'DropDownAPIScreen';
+
+  @override
+  State<DropDownAPIScreen> createState() => _DropDownAPIScreenState();
+}
+
+class _DropDownAPIScreenState extends State<DropDownAPIScreen> {
+  
+  List<DropDownModel> postsList = [];
+  
+  Future<List<DropDownModel>> getPost() async  {
+    try{
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+      var body = jsonDecode(response.body.toString());
+
+      if(response.statusCode == 200){
+        postsList.clear();
+
+        return body.map((e){
+          final map = e as Map<String, dynamic> ;
+          return DropDownModel(
+            id: map['id'],
+            title: map['title'],
+            userId: map['userId'],
+            body: map['body'],
+          );
+        }).toList();
+      }
+    } on SocketException{
+      throw Exception('no internet');
+    }
+    throw Exception('Error in API fetching');
+  }
+  
+  var selectedValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dropdown Api'),
+      ),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FutureBuilder<List<DropDownModel>>(
+                future: getPost(),
+                builder: (context, snapshot){
+                  if(snapshot.hasData){
+                    return DropdownButton(
+                      isExpanded: true,
+                      value: selectedValue,
+                      hint: Text('Select Value'),
+                        items: snapshot.data!.map((e) {
+                          return DropdownMenuItem(
+                            value: e.id.toString(),
+                              child: Text(e.id.toString()),
+                          );
+                        }).toList(),
+                        onChanged: (value){
+                          selectedValue = value;
+                          setState(() {
+
+                          });
+                        }
+                    );
+                  }else{
+                    return CircularProgressIndicator();
+                  }
+                }
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_working_with_api/drop_down_api_screen.dart';
 import 'package:flutter_practice_working_with_api/example_five_screen.dart';
 import 'package:flutter_practice_working_with_api/example_four_screen.dart';
 import 'package:flutter_practice_working_with_api/example_three_screen.dart';
@@ -29,7 +30,8 @@ class MyApp extends StatelessWidget {
       // initialRoute: ExampleFourScreen.screen_id,
       // initialRoute: ExampleFiveScreen.screen_id,
       // initialRoute: SignUpScreen.screen_id,
-      initialRoute: UploadImageScreen.screen_id,
+      // initialRoute: UploadImageScreen.screen_id,
+      initialRoute: DropDownAPIScreen.screen_id,
       routes: {
         HomeScreen.screen_id: (context) => HomeScreen(),
         ExampleTwoScreen.screen_id: (context) => ExampleTwoScreen(),
@@ -38,6 +40,7 @@ class MyApp extends StatelessWidget {
         ExampleFiveScreen.screen_id: (context) => ExampleFiveScreen(),
         SignUpScreen.screen_id: (context) => SignUpScreen(),
         UploadImageScreen.screen_id: (context) => UploadImageScreen(),
+        DropDownAPIScreen.screen_id: (context) => DropDownAPIScreen(),
       },
     );
   }
